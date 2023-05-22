09cc5c4dfd4a8475f842066f3aa1c4a3e8ad7937
Timur <dutyscbars@gmail.com>
2023-05-22 11:03:26 +0300

    COMMIT AND PUSH

--- BASE_PAGE.py
+++ BASE_PAGE.py
@@ -12,14 +12,14 @@ class BasePage:
         ActionChains(self.driver).move_to_element(element)
         return ActionChains(self.driver).context_click(element)
 
-    def find_element_pb(self, time=30):
+    def find_element_pb(self, time=60):
         LOCATOR_DIARY_PB = (By.XPATH, '//div[@class="progress-bar"]')
         return WebDriverWait(self.driver, time).until(ES.invisibility_of_element_located(LOCATOR_DIARY_PB), message='Not found element progress bar')
 
-    def find_element(self, locator, time=15):
+    def find_element(self, locator, time=20):
         return WebDriverWait(self.driver, time).until(ES.presence_of_element_located(locator), message=f'Not found {locator}')
 
-    def find_clickable_elements(self, locator, time=15):
+    def find_clickable_elements(self, locator, time=20):
         return WebDriverWait(self.driver, time).until(ES.element_to_be_clickable(locator), message=f'Not found {locator}')
 
     def get_screenshots(self, name):
