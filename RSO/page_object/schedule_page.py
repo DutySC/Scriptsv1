777c9f08420a195ad849060a93fc6f62eeeaed20
DutySC <dutyscbars@gmail.com>
2023-05-30 13:09:34 +0300

    COMMIT AND PUSH

--- RSO/page_object/schedule_page.py
+++ RSO/page_object/schedule_page.py
@@ -5,12 +5,13 @@ from selenium.webdriver.common.by import By
 
 class locators_schedule:
     LOCATOR_SCHEDULE_1 = (By.XPATH, '//span[contains(text(), "Регистратура")]')
-    LOCATOR_SCHEDULE_2 = (By.XPATH, '//tbody/tr[4]//span[1][contains(text(), "Расписание")]')
+    LOCATOR_SCHEDULE_2 = (By.XPATH, '//tr[3]//span[contains(text(), "Расписание")]')
     LOCATOR_REG_CONTAINER = (By.XPATH, '//div[@id = "RegContainer"]')
     LOCATOR_FILTER = (By.XPATH, '//tbody/tr[3]//div[2]/div[3]')
     LOCATOR_FILTER_CABINET = (By.XPATH, '//body[1]//tbody[1]/tr[3]//tr[3]/td[1]//input[1]')
+    LOCATOR_FILTER_DOC = (By.XPATH, '//div[1]/div[1]//tr[3]/td[2]//input[1]')
     LOCATOR_SEARCH_1 = (By.XPATH, '//body[1]//tr[3]//td[contains(text(), "Найти")]')
-    LOCATOR_CHOOSE_SCHEDULE = (By.XPATH, '//a[contains(text(), "Врач О.Г.")]')
+    LOCATOR_CHOOSE_SCHEDULE = (By.XPATH, '//a[contains(text(), "Тестирование П.")]')
     LOCATOR_NEXT_PAGE = (By.XPATH, '//body[1]/div[7]//table[2]//img[1]')
     LOCATOR_WRITE_TO_DOCTOR = (By.XPATH, '//tbody/tr[4]/td//a[contains(text(), "Записать")]')
     LOCATOR_CARD_NUMBER = (By.XPATH, '//body[1]//div[9]//td[5]//input[1]')
@@ -43,6 +44,8 @@ class schedule(BasePage):
             self.find_element(locators_schedule.LOCATOR_FILTER).click() # шторка вниз
             search_string_2 = self.find_element(locators_schedule.LOCATOR_FILTER_CABINET) # ввод кабинета
             search_string_2.send_keys(prm.polyclinic) # указать кабинет
+            search_string_3 = self.find_element(locators_schedule. LOCATOR_FILTER_DOC)  # ввод врача
+            search_string_3.send_keys(prm.doctor)  # указать врача
             self.find_element(locators_schedule.LOCATOR_SEARCH_1).click() # кнопка "Найти"
             self.find_element_pb() # прогрессбар
             self.find_element(locators_schedule.LOCATOR_CHOOSE_SCHEDULE).click() # выбор врача для тестирования "Врач О.Г.Терапия"
